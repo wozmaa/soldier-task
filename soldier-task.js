ea63ff4217a686ab3a6ec644f0c092349edcf211
Morz <morz@Morzs-MacBook-Pro.local>
2022-05-18 12:08:43 +0300

    israil

--- soldier-task.js
+++ soldier-task.js
@@ -3,19 +3,33 @@ const soldier = {
     health: 10,
     gun: {
         gunName: 'tapch',
-        cartridges: 8,
+        cartridges: 30,
     },
     supplies: 8,
 
     shoot: function () {
-
+        --this.cartridges;
+        if(this.cartridges < 1){
+            console.log('обойма пуста. Перезаредитесь')
+        } else {
+            console.log('бах-бах')
+        }
     },
 
     recharge: function () {
-
+        this.cartridges = 30;
+        --this.supplies;
+        if(this.supplies < 1){
+            console.log('не осталось припасов')
+        } else {
+            console.log('перезарядка...')
+        }
     },
 
     hurt: function () {
-
+        --this.health;
+        if(this.health < 1){
+            console.log('Ты проиграл')
+        }
     }
 }

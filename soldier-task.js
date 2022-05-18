1de0480b00278c6c8586682d908a20fd162ad845
Morz <morz@Morzs-MacBook-Pro.local>
2022-05-18 11:00:25 +0300

    israil

---
+++ soldier-task.js
@@ -0,0 +1,21 @@
+const soldier = {
+    name: 'khamzat',
+    health: 10,
+    gun: {
+        gunName: 'tapch',
+        cartridges: 8,
+    },
+    supplies: 8,
+
+    shoot: function () {
+
+    },
+
+    recharge: function () {
+
+    },
+
+    hurt: function () {
+
+    }
+}
